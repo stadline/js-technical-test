29041a69e6146563fcf1d41dff0d1e541b0a0bbd
cmnt <dufour.clement@gmail.com>
2018-01-21 23:42:27 +0100

    Add github api call

---
+++ src/utils/github-api.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'https://api.github.com'
+
+export const getIssueInfos = async(path) => {
+    const url = `${BASE_URL}/repos/${path}`;
+
+    try {
+        const response = await fetch(url, {method: 'get'});
+        if (response.status !== 200) {
+            throw new Error(response.status);
+        }
+
+        const data = await response.json();
+
+        return data;
+
+    } catch (error) {
+        console.error(error);
+    }
+
+}
