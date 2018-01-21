70b1e0ca7398239c00c1b523edffbc97b796612c
cmnt <dufour.clement@gmail.com>
2018-01-22 00:54:08 +0100

    Show title

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import * as github from './utils/github-api.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  async componentDidMount() {
+    const path = new URL(window.location.href).pathname;
+    const info = await github.getIssueInfos(path);
+    this.setState({
+      title: info.title + ' #' + info.number
+    });
+
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{this.state && this.state.title}</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
       </div>
     );
--- src/utils/github-api.js
+++ src/utils/github-api.js
@@ -1,12 +1,12 @@
 const BASE_URL = 'https://api.github.com'
 
 export const getIssueInfos = async(path) => {
-    const url = `${BASE_URL}/repos/${path}`;
+    const url = `${BASE_URL}/repos${path}`;
 
     try {
         const response = await fetch(url, {method: 'get'});
         if (response.status !== 200) {
-            throw new Error(response.status);
+            throw new Error(`Check the path ! ${response.status}`);
         }
 
         const data = await response.json();
