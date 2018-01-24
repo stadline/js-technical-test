a642fc0e35e41be76cb55968f6151e924869189a
cmnt <dufour.clement@gmail.com>
2018-01-24 21:48:33 +0100

    Add author position

--- src/App.js
+++ src/App.js
@@ -5,49 +5,59 @@ import * as github from './utils/github-api.js'
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.fetch
   }
 
   async componentDidMount() {
     const path = new URL(window.location.href).pathname;
-    await this.initIssueInfos(path);
-    await this.initComments(path);
+    await this.fetchIssue(path);
   }
 
-  async initIssueInfos(path) {
+  async fetchIssue(path) {
     const issueInfo = await github.getIssueInfos(path);
+    const comments = await github.getComments(path);
     this.setState({
       title: issueInfo.title + ' #' + issueInfo.number,
       author: {
         login: issueInfo.user.login,
         id: issueInfo.user.id
-      }
+      },
+      comments: comments
     });
   }
 
-  async initComments(path) {
-    const comments = await github.getComments(path);
-    this.setState({comments: comments});
+  render() {
+    if (!this.state) {
+      return <p>
+        Loading...
+      </p>;
+    }
+    return (<Github comments={this.state.comments} author={this.state.author}/>);
   }
+}
+export default App;
+
+class Github extends Component {
 
   renderComments() {
-    return this.state.comments && this
-      .state
+    return this
+      .props
       .comments
       .map(comment => <Comment
         key={comment.id}
         comment={comment.body}
-        avatar={comment.user.avatar_url}/>);
+        avatar={comment.user.avatar_url}
+        isAuthor={comment.user.id === this.props.author.id}/>);
   }
 
   render() {
-
     return (
       <div className="App">
-        <Header className="App-header" value={this.state.title}/>
+        <Header className="App-header" value={this.props && this.props.title}/>
 
         <div className="App-thread">
-          <h2 className>Conversation with {this.state.author && this.state.author.login}
+          <h2>
+            Conversation with {this.props.author.login}
           </h2>
           <ul>
             {this.renderComments()}
@@ -58,13 +68,12 @@ class App extends Component {
     );
   }
 }
-export default App;
 
 class Header extends Component {
   render() {
     return (
       <header className="App-header">
-        <h1 className="App-title">{this.props.value}</h1>
+        <h1>{this.props.value}</h1>
       </header>
     );
   }
@@ -72,15 +81,18 @@ class Header extends Component {
 
 class Comment extends Component {
   render() {
+    const classPosition = this.props.isAuthor
+      ? ' right'
+      : ' left';
     return (
       <li className="App-thread-container">
-        <div className="App-thread-container-avatarcontainer">
+        <div className={'App-thread-container-avatarcontainer' + classPosition}>
           <img
             src={this.props.avatar}
             alt='avatar'
-            className="App-thread-container-avatar"/>
+            className={"App-thread-container-avatar" + classPosition}/>
         </div>
-        <div className="App-thread-container-comment">
+        <div className={"App-thread-container-comment" + classPosition}>
           <p >
             {this.props.comment}
           </p>
